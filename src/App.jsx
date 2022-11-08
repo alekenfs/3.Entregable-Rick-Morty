888be3a62eac21ae6d4285513ad93c518c8a8a71
alekenfs <114787077+alekenfs@users.noreply.github.com>
2022-11-07 23:39:28 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import ResidentInfo from './Components/ResidentInfo'
+import Location from './Components/Location'
+
+function App() {
+  const [location, setLocation] = useState({})
+  const [locationID, setLocationID] = useState("Type a number 1-126")
+
+
+
+  useEffect(()=>{
+  const ramdomId= Math.floor(Math.random()*126)+1;
+axios.get(`https://rickandmortyapi.com/api/location/${ramdomId}`)
+.then(res=>(setLocation(res.data)))
+}, [])
+
+const searchLocation=  () =>{
+  axios.get(`https://rickandmortyapi.com/api/location/${locationID}`)
+  .then(res=>(setLocation(res.data)))
+  if(locationID>126){alert("NO EXISTEN PLANETAS")}
+
+}
+
+console.log(location);
+
+  return (
+    <div className="App">
+      <img className='headerimg' src="../src/assets/tumblr_pjug61Pxx81xgc3e1o3_1280.jpg" alt="" />
+    <b>Rick and Morty</b>
+      <div className='barContainer'><input type="text" value={locationID} onChange={e=>setLocationID(e.target.value)}/> <button onClick={searchLocation}>Buscar</button></div>
+      <Location
+      location={location}
+      />
+<br />
+<h1>Residents</h1>
+
+<ul>
+{location.residents?.map(characters=>
+
+  <ResidentInfo key={characters}
+  characters= {characters}
+
+
+  />
+  )}
+</ul>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ResidentInfo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const ResidentInfo = ({ characters }) => {
+    const [citizen, setcityzen] = useState([])
+    useEffect(() => {
+        axios.get(`${characters}`)
+            .then(res => (setcityzen(res.data)))
+    }, [])
+
+
+    console.log(citizen);
+    return (
+        <div className='residentsContainer'>
+            <li className='residentInfo'>
+                <div  style={citizen.status=== "Alive"? {background:"green"}:{backgroundColor:"red"}} className="status-color"></div> <h3 className='status'>Status: {citizen.status}</h3>
+                <img src={citizen.image} alt="" /> 
+                <h2>{citizen.name}</h2>
+                <h4 id='Race'>Race: </h4>
+                <h3 >{citizen.species}</h3>
+                <h4>Origin: </h4>
+                <h3 >{citizen.origin?.name}</h3>
+                <h4>Appearences in Episodes:</h4>
+                <h3> {citizen.episode?.length}</h3>
+
+                
+            </li>
+        </div>
+    );
+};
+
+export default ResidentInfo;
