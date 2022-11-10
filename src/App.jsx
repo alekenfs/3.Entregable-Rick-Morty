0f86efc5b15527db4024fe4d653a43cc396913bf
alekenfs <114787077+alekenfs@users.noreply.github.com>
2022-11-10 00:20:29 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,42 +10,45 @@ function App() {
 
 
 
-  useEffect(()=>{
-  const ramdomId= Math.floor(Math.random()*126)+1;
-axios.get(`https://rickandmortyapi.com/api/location/${ramdomId}`)
-.then(res=>(setLocation(res.data)))
-}, [])
+  useEffect(() => {
+    const ramdomId = Math.floor(Math.random() * 126) + 1;
+    axios.get(`https://rickandmortyapi.com/api/location/${ramdomId}`)
+      .then(res => (setLocation(res.data)))
+  }, [])
 
-const searchLocation=  () =>{
-  axios.get(`https://rickandmortyapi.com/api/location/${locationID}`)
-  .then(res=>(setLocation(res.data)))
-  if(locationID>126){alert("NO EXISTEN PLANETAS")}
+  const searchLocation = () => {
+    axios.get(`https://rickandmortyapi.com/api/location/${locationID}`)
+      .then(res => (setLocation(res.data)))
+    if (locationID > 126) { alert("NO EXISTEN PLANETAS") }
 
-}
+  }
 
-console.log(location);
+  console.log(location);
 
   return (
     <div className="App">
       <img className='headerimg' src="https://64.media.tumblr.com/dd09fb72824950040154dfd1a094c945/tumblr_pjug61Pxx81xgc3e1o3_1280.jpg" alt="" />
-    <b>Rick and Morty</b>
-      <div className='barContainer'><input type="text" value={locationID} onChange={e=>setLocationID(e.target.value)}/> <button onClick={searchLocation}>Buscar</button></div>
+      <b>Rick and Morty</b>
+      <div className='barContainer'><input type="text" value={locationID} onChange={e => setLocationID(e.target.value)} /> <button onClick={searchLocation}>Buscar</button></div>
       <Location
-      location={location}
+        location={location}
       />
-<br />
-<h1>Residents</h1>
+      <br />
+      <h1>Residents</h1>
 
-<ul>
-{location.residents?.map(characters=>
+      <ul>
+        <div className='container-grid'>
+          {location.residents?.map(characters =>
 
-  <ResidentInfo key={characters}
-  characters= {characters}
+            <ResidentInfo key={characters}
+              characters={characters}
 
 
-  />
-  )}
-</ul>
+            />
+          )}
+        </div>
+
+      </ul>
     </div>
   )
 }
